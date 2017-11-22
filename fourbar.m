501023cd55e636c96a5a431ad6b8e531823b6725
andfroswe <gummianka88@gmail.com>
2017-11-22 15:44:13 +0100

    Initial commit

---
+++ fourbar.m
@@ -0,0 +1,11 @@
+% Fourbar solver
+
+% Geometric parameters
+rf = 100 % Frame length [mm]
+rl = 50 % Link length [mm]
+rs = 60 % Shank length [mm]
+rj = 40 % Jaw length [mm]
+
+disp 'Starting Four bar calculator...'
+printf('
+
